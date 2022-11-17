466cf28b7ff7f7b03c4727c9d5111c0c29a9a9ec
Yethu <yethukichu55@gmail.com>
2022-11-17 21:00:35 +0530

    router added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/userRoute');
 
 dotenv.config()
 const app = express();
 
+//middleware
 app.use(express())
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
