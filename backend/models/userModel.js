49891a09598cd12c7389b31c74ca34c59fa4fb70
Yethu <yethukichu55@gmail.com>
2022-11-17 20:52:16 +0530

    userModel added

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+    },
+    email: {
+        type: String,
+        required:[true, 'Please enter an email'],
+        unique: true,
+        trim : true,
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+        'Please enter a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
+    },
+    photo: {
+        type: String,
+        default: 'https://res.cloudinary.com/dxqjyqz8f/image/upload/v1621234567/avatars/default-avatar.png'
+    },
+    phone: {
+        type: String,
+        required: [true, 'Please enter a phone number'],
+    },
+    bio: {
+        type: String,
+        default: 'Hey there! I am using Social Media App.'
+    },
+     
+
+
+}, {timestamps: true})
+
+module.exports = mongoose.model('User', userSchema)
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const dotenv = require('dotenv');
 dotenv.config()
 const app = express();
 
+app.use(express())
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL)
