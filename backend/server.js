91668c9557b94357b39be4e33b5f904395f91e2e
Yethu <yethukichu55@gmail.com>
2022-11-17 20:29:30 +0530

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config()
 const app = express();
 
-mongoose.connect(``)
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('MongoDB Connected'))
+.then(() => app.listen(port, () => console.log(`Server running on port: ${port}`)))
