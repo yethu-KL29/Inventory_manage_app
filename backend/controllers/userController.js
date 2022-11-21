0fd91470793132dac3b90954e8bc4e24eb4d29d4
Yethu <yethukichu55@gmail.com>
2022-11-21 19:46:36 +0530

    resiter controller updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,27 @@
 const User = require('../models/userModel')
 
 
-const registerUser = async (req, res) => {
+const registerUser = async(req, res) => {
    
-        const {name, email, password} = req.body;
-        const user = await User.findOne({email});
-        if(user) return res.status(400).json({msg: 'The email already exists.'})
-        if(password.length < 6) return res.status(400).json({msg: 'Password is at least 6 characters long.'})
-       
-        try {
-        const user = new User({name, email, password});
-        await user.save();
-
-        }catch(err){
-            return res.status(500).json({msg: err.message})
+    const{ name , email ,password} =req.body
+    let user;
+    user = await User.findOne({email});
+    if(user){
+        return res.status(400).json({success: false, message: 'User already exists'})
+    }
+         try {
+            user = new User({
+                name,
+                email,
+                password
+             })
+            await user.save()
+        } catch (error) {
+            console.log(error)
         }
-
-if(!user) return res.status(400).json({msg: 'Invalid credentials.'})
-res.json({msg: 'Register Success!',user})
+        if(!user){
+            return res.status(500).json({message:"internal server error"})
+        }
+        return res.status(201).json({user})
 }
 exports.registerUser = registerUser
