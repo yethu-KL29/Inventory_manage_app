b5881abc858881937141831cbb5e5db49c35ca9a
Yethu <yethukichu55@gmail.com>
2022-11-22 20:44:00 +0530

    token created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,16 @@
 const User = require('../models/userModel')
-
+const jwt = require('jsonwebtoken')
 const registerUser = async(req, res) => {
    
     const{ name , email ,password} =req.body
+    const generateToken = (id) => {
+        return jwt.sign({id}, process.env.JWT_SECRETKEY, {
+            expiresIn: '30d'
+        })
+    }
+
+
+
     if(!name || !email || !password){
         return res.status(400).json({msg: "Please fill in all fields"})
     }
@@ -17,13 +25,17 @@ const registerUser = async(req, res) => {
                 email,
                 password
              })
+            
             await user.save()
+            const token= generateToken(user._id)
+            console.log(token)
         } catch (error) {
             console.log(error)
         }
         if(!user){
             return res.status(500).json({message:"internal server error"})
         }
+      
         return res.status(201).json({user})
 }
 exports.registerUser = registerUser
