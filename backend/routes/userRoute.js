959f7c71c5680b315fef840697d20d2f4c31a58b
Yethu <yethukichu55@gmail.com>
2022-11-23 20:16:54 +0530

    middleware added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, login, logout } = require('../controllers/userController');
+const { registerUser, login, logout, getUser } = require('../controllers/userController');
+const protect = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',login);
 router.post('/logout',logout);
+router.get('/getuser',protect,getUser)
 
 module.exports = router;
