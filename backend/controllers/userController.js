a2ccde398d93baadfda7ea1b0cc899330ffffe09
Yethu <yethukichu55@gmail.com>
2022-11-26 21:25:55 +0530

    reset password functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -187,10 +187,10 @@ const forgotPassword =async (req, res) => {
     }
   
     // Delete token if it exists in DB
-    // let token = await Token.findOne({ userId: user._id });
-    // if (token) {
-    //   await token.deleteOne();
-    // }
+    let token = await Token.findOne({ userId: user._id });
+    if (token) {
+      await token.deleteOne();
+    }
   
     // Create Reste Token
     let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
@@ -231,7 +231,37 @@ const forgotPassword =async (req, res) => {
     throw new Error("Email not sent, please try again");
     }
 }
-
+// Reset Password
+const resetPassword =async (req, res) => {
+    const { password } = req.body;
+    const { resetToken } = req.params;
+  
+    // Hash token, then compare to Token in DB
+    const hashedToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+  
+    // fIND tOKEN in DB
+    const userToken = await Token.findOne({
+      token: hashedToken,
+      expiresAt: { $gt: Date.now() },
+    });
+  
+    if (!userToken) {
+      res.status(404);
+      throw new Error("Invalid or Expired Token");
+    }
+  
+    // Find user
+    const user = await User.findOne({ _id: userToken.userId });
+    user.password = password;
+    await user.save();
+    res.status(200).json({
+      message: "Password Reset Successful, Please Login",
+    });
+}
+  
 
     exports.registerUser = registerUser
     exports.login = login
